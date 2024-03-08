05c5f6206dd137869dc0912115c5f6daa978ea28
aashishChakra <ashishchakradhar00@gmail.com>
2024-03-08 15:24:32 +0545

    edited home page and admin login pannel

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "My Quiz Admin"
+admin.site.site_title = "My Quiz Admin Portal"
+admin.site.index_title = "Welcome to My Quiz"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz.urls')),
