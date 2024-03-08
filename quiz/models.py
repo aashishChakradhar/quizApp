946c7d4a46338d2bbdb58048eea0b3c0b83e363e
aashishChakra <ashishchakradhar00@gmail.com>
2024-03-08 16:46:45 +0545

    added database for question and contact form

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    fname = models.CharField(max_length=25)
+    lname = models.CharField(max_length=25)
+    phone = models.CharField(max_length=12)
+    email = models.CharField(max_length=50)
+    comment = models.TextField()
+    date = models.DateField()
+
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    correct_answer = models.CharField(max_length=20)
+    option_1 = models.CharField(max_length=20)
+    option_2 = models.CharField(max_length=20)
+    option_3 = models.CharField(max_length=20)
--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render,HttpResponse
-# from quiz.models
+from datetime import datetime
+from quiz.models import Contact, Question
+
 # Create your views here.
 def home(request):
     return render (request,'home.html')
 def about(request):
     return render (request,'about.html')
 def contact(request):
+    if request.method == "POST":
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        comment = request.POST.get('comment')
+        contact = Contact(fname = fname, lname = lname, email = email, phone=phone, comment = comment, date = datetime.today())
+        contact.save()
     return render (request,'contact.html')
 def question(request):
+    if request.method == 'POST':
+        question = request.POST.get('question')
+        correct_answer = request.POST.get('correct_answer')
+        option_1 = request.POST.get('option_1')
+        option_2 = request.POST.get('option_2')
+        option_3 = request.POST.get('option_3')
+        question = Question(question=question, correct_answer=correct_answer, option_1=option_1, option_2=option_2,option_3=option_3)
+        question.save()
     return render (request,'question.html')
