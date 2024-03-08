6f6574403016fd0624044f4f9067469a6d31b622
aashishChakra <ashishchakradhar00@gmail.com>
2024-03-08 16:27:53 +0545

    added contact page.

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,6 +5,7 @@ from quiz import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('question', views.question, name='question'),
     path('about', views.about, name='about'),
+    path('question', views.question, name='question'),
+    path('contact', views.contact, name='contact'),
 ]
