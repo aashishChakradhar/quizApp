e2c6ec282dad9791bce8c3d9c26d5e6107d3e9a3
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-07-30 16:54:29 +0545

    setting up api for token access

--- quiz/api_url.py
+++ quiz/api_url.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from quiz import api_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 app_name = 'api'
+
 urlpatterns = [
+    # for api testing token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     # Category Endpoints
     path('categories/', api_views.CategoryListAPIView.as_view(), name='category-list'),
     path('categories/create/', api_views.CategoryCreateAPIView.as_view(), name='category-create'),
