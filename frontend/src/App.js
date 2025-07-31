2328e27c8bd1bf853835431ecf017b0f71aba15b
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-07-31 17:35:27 +0545

    creating react app for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.css';
+import Category from "./components/category.jsx";
+function App() {
+  return (
+    <div className="App">
+      <Category />
+    </div>
+  );
+}
+
+export default App;
