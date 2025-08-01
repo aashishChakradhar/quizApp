ed2495f005b5b3cf1c6321b102a181c82672d5f6
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-01 19:42:09 +0545

    file to view categorys

---
+++ frontend/src/pages/Category.jsx
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+export default function Category(props){
+    const [category,setCategory] = useState([]);
+    useEffect( () => {
+        axios.get(`${process.env.REACT_APP_API_URL}/categories/`)
+        .then(res => setCategory(res.data))
+        .catch(err => console.error(err))
+    },[]);
+    
+    return <div id="category-container">
+        <h1>Category</h1>
+        <ul>
+            {category.map( cat => (
+            <li key={cat.uid}>{cat.category_name}</li>
+        ))}
+        </ul>
+    </div>
+}
