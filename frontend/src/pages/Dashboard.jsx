5aa173b7a50dc0728b7d7555bc2db57560341e0c
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-01 19:42:32 +0545

    updating dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,23 @@
-
 import React from 'react';
 import { logout } from '../services/authService';
+import {useNavigate,Link} from 'react-router-dom';
+ 
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+  function handleLogout(){
+    logout();
+    navigate('/login')
+  }
   return (
-    <div>
+    <div id='dashboard-container'>
       <h2>Welcome to Dashboard</h2>
-      <button onClick={logout}>Logout</button>
+      <ul>
+        <li><Link to="/test">Take exam</Link></li>
+        <li><Link to="/category">View Report</Link></li>
+        <li><Link to="/category">View Exam</Link></li>
+      </ul>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
