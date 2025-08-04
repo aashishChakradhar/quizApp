e39000f10de457e991518fb7efeb287f19a968ce
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-04 16:39:19 +0545

    dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import { logout } from '../services/authService';
-import {useNavigate,Link} from 'react-router-dom';
- 
+import { logout } from "../services/authService";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Dashboard() {
   const navigate = useNavigate();
-  function handleLogout(){
+  function handleLogout() {
     logout();
-    navigate('/login')
+    navigate("/login");
   }
   return (
-    <div id='dashboard-container'>
+    <div id="dashboard-container">
       <h2>Welcome to Dashboard</h2>
-      <ul>
-        <li><Link to="/test">Take exam</Link></li>
-        <li><Link to="/category">View Report</Link></li>
-        <li><Link to="/category">View Exam</Link></li>
-      </ul>
-      <button onClick={handleLogout}>Logout</button>
+      dashboar goes here
     </div>
   );
 }
