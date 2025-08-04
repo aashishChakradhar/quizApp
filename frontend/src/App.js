87be3c9568c3f557bba5c73be256c2d058d333bf
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-04 18:18:03 +0545

    adding signup route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import Dashboard from "./pages/Dashboard";
 import Category from "./pages/Category";
 import RecordView from "./pages/Records";
 import Test from "./pages/Exam";
+import Signup from "./pages/Signup";
 import PrivateRoute from "./routes/PrivateRoute";
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/"
