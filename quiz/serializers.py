3d3f4ecb36ea364caf3eab00241f737384bfd9b2
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-04 18:17:14 +0545

    managing functionality for creating user

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,11 +1,36 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from .models import Category, Records, Question, Answer
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password', 'password2']
+    
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+        )
+        return user
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['uid', 'category_name']
 
 class RecordsSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
@@ -17,11 +42,12 @@ class RecordsSerializer(serializers.ModelSerializer):
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
-        fields = '__all__'
+        fields = ['uid', 'answer', 'is_correct']
 
 class QuestionSerializer(serializers.ModelSerializer):
     question_answer = AnswerSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
+    user = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Question
