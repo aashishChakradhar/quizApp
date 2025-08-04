96126e010cc4cfecd4db117c2157bea732a4b5aa
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-04 16:38:16 +0545

    modification to view sidebar, header, footer

--- frontend/src/routes/PrivateRoute.js
+++ frontend/src/routes/PrivateRoute.js
@@ -1,6 +1,19 @@
-import { Navigate } from 'react-router-dom';
-import { isAuthenticated } from '../services/authService';
+import { Navigate } from "react-router-dom";
+import { isAuthenticated } from "../services/authService";
+import Header from "../components/header";
+import Sidebar from "../components/sidebar";
+import Footer from "../components/footer";
 
 export default function PrivateRoute({ children }) {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
+  if (!isAuthenticated()) {
+    <Navigate to="/login" />;
+  }
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      {children}
+      <Footer />
+    </>
+  );
 }
