d0a4aece0ff30dd5471aae6aefcbc3b9984e2bfd
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-06 21:11:35 +0545

    adding greeetings and title navigation

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
 export default function Header(props) {
-  return <header>Quizzy Quiz</header>;
+  const greeting =
+    new Date().getHours() < 12
+      ? "Good Morning"
+      : new Date().getHours() < 16
+      ? "Good Afternoon"
+      : "Good Evening";
+  return (
+    <header>
+      <Link id="title" to="/dashboard">
+        Quizzy Quiz
+      </Link>
+      <div id="greeting">
+        {greeting},{props.user}
+      </div>
+    </header>
+  );
 }
