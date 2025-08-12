5f9648111a736fd438c238ccf633be6a649250a6
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-12 23:47:56 +0545

    filter result based on topics

--- frontend/src/pages/Records.jsx
+++ frontend/src/pages/Records.jsx
@@ -2,29 +2,63 @@ import { useState, useEffect } from "react";
 import api from "../api/token.js";
 export default function RecordView(porps) {
   const [records, setRecords] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState([]);
   useEffect(() => {
     api
       .get(`${process.env.REACT_APP_API_URL}/records/view/`)
-      .then((res) => setRecords(res.data))
+      .then((res) => {
+        setRecords(res.data);
+        setFiltered(res.data);
+        const uniqueCategories = Array.from(
+          new Set(res.data.map((record) => record.category))
+        );
+        setCategories(uniqueCategories);
+      })
       .catch((err) => console.error(err));
   }, []);
+  function handleFilter(e) {
+    const category = e.target.value;
+    setSelectedCategory(category);
 
+    if (category === "") {
+      setFiltered(records);
+    } else {
+      const filtered = records.filter((record) => record.category === category);
+      setFiltered(filtered);
+    }
+  }
   return (
     <div id="display-container">
-      Hello records
+      <div id="filter">
+        Filter:
+        <select
+          style={{ marginRight: "auto" }}
+          name="category"
+          id="category"
+          value={selectedCategory}
+          onChange={handleFilter}
+        >
+          <option value="">-- All --</option>
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
       <table>
         <thead>
           <tr>
-            <th>User</th>
             <th>Category</th>
             <th>Score</th>
             <th>Exam Date</th>
           </tr>
         </thead>
         <tbody>
-          {records.map((item, index) => (
+          {filtered.map((item, index) => (
             <tr key={index}>
-              <td>{item.user}</td>
               <td>{item.category}</td>
               <td>{item.score}</td>
               <td>{item.created_at}</td>
