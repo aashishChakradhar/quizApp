460f9b2a8d23705ec0c2d2b6da2d710a58d8340a
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-12 23:47:03 +0545

    filtering the graph for month

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,42 @@
 import { logout } from "../services/authService";
+import { useState, useEffect } from "react";
+import api from "../api/token.js";
+import BarGraph from "../components/bargraph";
 import { useNavigate, Link } from "react-router-dom";
 
 export default function Dashboard() {
-  const navigate = useNavigate();
-  function handleLogout() {
-    logout();
-    navigate("/login");
-  }
+  const [records, setRecords] = useState([]);
+  const [activeRecords, setActiveRecords] = useState([]);
+  useEffect(() => {
+    api
+      .get(`${process.env.REACT_APP_API_URL}/records/view/`)
+      .then((res) => {
+        setRecords(res.data);
+        const current_year = new Date().getFullYear();
+        const current_month = new Date().getMonth();
+        const filtered = res.data.filter((record) => {
+          const recordDate = new Date(record.created_at);
+          return (
+            recordDate.getMonth() === current_month &&
+            recordDate.getFullYear() === current_year
+          );
+        });
+        // Transform records to add a "name" field for the chart X-axis
+        const transformed = filtered.map((record) => ({
+          name: `${record.category}`, // customize label
+          score: record.score,
+          uid: record.uid,
+        }));
+
+        setActiveRecords(transformed);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div id="display-container">
-      <h2>Welcome to Dashboard</h2>
-      dashboar goes here
+      Track your Records
+      <BarGraph data={activeRecords} />
     </div>
   );
 }
