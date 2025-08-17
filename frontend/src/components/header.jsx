ae65e7dca444c73c4d1b87e4ca473354dfb977e3
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-08-17 22:19:16 +0545

    adding user name for greeting

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,22 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import api from "../api/token.js";
+import { useState, useEffect } from "react";
 export default function Header(props) {
+  const navigate = Navigate;
+  const [user, setUser] = useState({ username: "" });
   const greeting =
     new Date().getHours() < 12
       ? "Good Morning"
       : new Date().getHours() < 16
       ? "Good Afternoon"
       : "Good Evening";
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await api.get(`${process.env.REACT_APP_API_URL}/profile/`);
+        setUser(res.data);
+      } catch (error) {
+        navigate("/login");
+      }
+    };
+    fetchUser();
+  });
   return (
     <header>
       <Link id="title" to="/dashboard">
         Quizzy Quiz
       </Link>
       <div id="greeting">
-        {greeting} {props.user}
+        {greeting}, {user.username.toUpperCase()}
       </div>
       <div id="profile">
         <Link to="/profile">
-          <span class="material-symbols-outlined">account_circle</span>
+          <span className="material-symbols-outlined">account_circle</span>
         </Link>
       </div>
     </header>
